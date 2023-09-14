9f3aaeabdbc773aee26acdbb29f139baa262339c
anubhav1772 <anubhavsingh55182@gmail.com>
2023-09-14 21:38:11 +0530

    published labeled axis at each cluster's centroid

--- src/axes_visual_tools.cpp
+++ src/axes_visual_tools.cpp
@@ -12,7 +12,7 @@ class RvizMarker
             : nh_(nh)
         {
             pcl_sub = nh_.subscribe("/pcl_centroids", 1, &RvizMarker::pclCallback, this);
-            visual_tools_.reset(new rviz_visual_tools::RvizVisualTools("world", "/rviz_visual_markers"));
+            visual_tools_.reset(new rviz_visual_tools::RvizVisualTools("camera_rgb_optical_frame", "/rviz_visual_markers"));
         }
     
     private:
@@ -32,8 +32,25 @@ class RvizMarker
             ROS_INFO("Clusters' Centroids Stats:");
             ROS_INFO("Height: %d", sp_pcl_centroids->height);
             ROS_INFO("Width: %d", sp_pcl_centroids->width);
-            ROS_INFO("Total data points : %d", sp_pcl_centroids->height*sp_pcl_centroids->width);
+
+            int size = sp_pcl_centroids->height*sp_pcl_centroids->width;
+            ROS_INFO("Total data points : %d", size);
             ROS_INFO("^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^");
+            
+            int i=0;
+            pcl::PointXYZRGB point;
+            Eigen::Isometry3d pose;
+            while(i<size)
+            {
+                point = (*sp_pcl_centroids)[i];
+                //pose.translation().x() = point.x;
+                //pose.translation().y() = point.y;
+                //pose.translation().z() = point.z;
+                pose.translation() = Eigen::Vector3d(point.x, point.y, point.z);
+                visual_tools_->publishAxisLabeled(pose, "centroid");
+                i++;
+            }
+            visual_tools_->trigger();
         }
 };
 
