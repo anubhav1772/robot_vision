f390aa9757cd8c384184014f073378aff9fc3e34
anubhav1772 <anubhavsingh55182@gmail.com>
2023-09-21 20:04:55 +0530

    Initial commit

---
+++ include/robot_vision/cloud_transformer.h
@@ -0,0 +1,35 @@
+#ifndef CLOUD_TRANSFORMER_H
+#define CLOUD_TRANSFORMER_H
+
+#include <ros/ros.h>
+#include <pcl_conversions/pcl_conversions.h>
+#include <pcl_ros/point_cloud.h>
+#include <pcl_ros/transforms.h>
+#include <pcl/point_cloud.h>
+#include <pcl/point_types.h>
+#include <sensor_msgs/PointCloud2.h>
+#include <tf/transform_listener.h>
+
+/*
+* This node transforms point cloud data from /camera_rgb_optical_frame frame to /world frame
+*/
+
+class CloudTransformer
+{
+public:
+  explicit CloudTransformer(ros::NodeHandle nh);
+  ~CloudTransformer();
+
+private:
+  ros::NodeHandle nh_;
+  ros::Subscriber pcl_sub_;
+  ros::Publisher pcl_pub_;
+  tf::TransformListener listener_;
+  sensor_msgs::PointCloud2::Ptr buffer_;
+  tf::StampedTransform transform;
+
+  void pclCallback(const sensor_msgs::PointCloud2ConstPtr& pcl_msg);
+
+};
+
+#endif
